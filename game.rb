726984d9e7c53e5e03fb8c87c920c82b148d1b77
Manoj <pmontu@gmail.com>
2014-11-05 16:10:12 +0530

    sliced shuffled grid locations

--- game.rb
+++ game.rb
@@ -2,71 +2,75 @@ require 'rubygems' # only necessary in Ruby 1.8
 require 'gosu'
 
 class MyWindow < Gosu::Window
-  def initialize
+	def initialize
 		super(640, 480, false)
-		self.caption = 'Hello World!'
+		self.caption = 'Butterfly Game'
+
 		@background_image = Gosu::Image.new(self, "media/SkyGround.jpg", true)
-		@flowers = Array.new
-		(1..4).each do |quadrant|
-			@flowers.push(Flower.new(self,quadrant))
-		end
-		@leaves = Array.new
-		(0..10).each do 
-			@leaves.push(Leaf.new(self))
+
+		@locations = []
+		(1..5).each do |yi|
+			li = []
+			(1..7).each do |xi|
+				@locations.push([640/8 * xi, 480/6 * yi])
+			end
 		end
 
+		@locations.shuffle!
 
-  end
+		@leaves = []
+		@locations[0..19].each do |point|
+			@leaves.push(Leaf.new(self,point[0],point[1]))
+		end
+
+		@flowers = []
+		@locations[20..26].each do |point|
+			@flowers.push(Flower.new(self,point[0],point[1]))
+		end
+
+	end
   
-  def update
+	def update
 
-  end
+	end
 
-  def draw
-	fx = 640.0 / @background_image.width
-	fy = 480.0 / @background_image.height
-  	@background_image.draw(0, 0, 0, fx, fy)
-  	@leaves.each { |leaf| leaf.draw }
-  	@flowers.each { |flower| flower.draw }
+	def draw
+		fx = 640.0 / @background_image.width
+		fy = 480.0 / @background_image.height
+	  	@background_image.draw(0, 0, 0, fx, fy)
+	  	@leaves.each { |leaf| leaf.draw }
+	  	@flowers.each { |flower| flower.draw }
 
-  	#draw_line(0, 480/2, Gosu::Color.argb(0xff000000), 640, 480/2, Gosu::Color.argb(0xff000000))
-  	#draw_line(640/2, 0, Gosu::Color.argb(0xff000000), 640/2, 480, Gosu::Color.argb(0xff000000))
-  end
+	  	@locations.each do |point|
+	  		#draw_line(point[0]-10, point[1]-10, Gosu::Color.argb(0xff000000), point[0]+10, point[1]+10, Gosu::Color.argb(0xff000000))
+	  		#draw_line(point[0]-10, point[1]+10, Gosu::Color.argb(0xff000000), point[0]+10, point[1]-10, Gosu::Color.argb(0xff000000))
+	  	end
+  	end
 end
 
 class Flower
-	def initialize(window, quadrant)
-		if quadrant == 1
-			@x = 640/4 - 100 + (rand * 100)
-			@y = 480/4 - 100 + (rand * 100)
-		elsif quadrant ==2
-			@x = 640*3/4 - 100 + (rand * 100)
-			@y = 480/4 - 100 + (rand * 100)
-		elsif quadrant ==3
-			@x = 640/4 - 100 + (rand *100)
-			@y = 480*3/4 - 100 + (rand * 100)
-		elsif quadrant ==4
-			@x = 640*3/4 - 100 + (rand *100)
-			@y = 480*3/4 - 100 + (rand * 100)
-		end
+	def initialize(window, x, y)
+		@x = x
+		@y = y
 		@img = Gosu::Image.new(window, "media/bagonia.png", true)
 	end
 
 	def draw
-		
-		@img.draw(@x, @y, 0, 0.25, 0.25)
+		scale = 0.15
+		@img.draw(@x - (scale * @img.width) / 2, @y - (scale * @img.height) / 2, 0, scale, scale)
 	end
 end
 
 class Leaf
-	def initialize(window)
-		@x = rand * 640
-		@y = rand * 480
+	def initialize(window, x, y)
+		@x = x
+		@y = y
 		@img = Gosu::Image.new(window, "media/autumn_leaves_PNG3611.png", true)
 	end
 
 	def draw
-		@img.draw(@x, @y, 0, 0.1, 0.1)
+		scale = 0.075
+		@img.draw(@x - (scale * @img.width) / 2, @y - (scale * @img.height) / 2, 0, scale, scale)
 	end
 end
 
