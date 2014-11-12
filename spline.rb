eb7575252865c0127aab7eb65e8d0f5665479b86
Manoj <pmontu@gmail.com>
2014-11-12 16:50:59 +0530

    spline interpolation aint going backwards

--- spline.rb
+++ spline.rb
@@ -1,15 +1,6 @@
 require 'gosu'
 
-require 'matrix'
- 
-def regress x, y, degree
-	x_data = x.map { |xi| (0..degree).map { |pow| (xi**pow).to_f } }
-
-	mx = Matrix[*x_data]
-	my = Matrix.column_vector(y)
-
-	((mx.t * mx).inv * mx.t * my).transpose.to_a[0]
-end
+require 'spliner'
 
 class MyWindow < Gosu::Window
 	def initialize
@@ -17,27 +8,28 @@ class MyWindow < Gosu::Window
 		self.caption = 'Butterfly Game Interpolation'
 		@background_image = Gosu::Image.new(self, "media/SkyGround.jpg", true)
 
-		@px = [100,200,300,300,300,200,100,100]
-		@py = [200,200,200,300,400,400,400,300]
-		@betas = regress @px, @py, 2
+		@px = [100,200,300]
+		@py = [200,300,200]
+		@my_spline = Spliner::Spliner.new @px, @py
+
 		@animation = Gosu::Image::load_tiles(self, "media/npc_butterfly__x1_fly-side_png_1354829525.png", -14, -6, false)
-		@x = 0
-		@y = 0
+		@x = 100
+		@y = 200
 
 		@arial = Gosu::Font.new(self, "Arial", 25)
 
 	end
 
 	def update
-		@x += 1
-		@y =  @betas[2] * @x**2 + @betas[1] * @x + @betas[0]
+		if @x < 300 then @x += 0.5 end
+		@y =  @my_spline[@x]
 	end
 
 	def draw
 	  	@background_image.draw(0, 0, 0,self.width.to_f/@background_image.width, self.height.to_f/@background_image.height)
 	  	img = @animation[Gosu::milliseconds / 100 % @animation.size];
 		img.draw(@x - img.width / 2 -10, @y - img.height / 2 - 10, 0)
-		8.times do |i|
+		3.times do |i|
 			draw_line(@px[i]-10,@py[i],Gosu::Color.new(0xff000000),@px[i]+10,@py[i],Gosu::Color.new(0xff000000),0)
 	  		draw_line(@px[i],@py[i]-10,Gosu::Color.new(0xff000000),@px[i],@py[i]+10,Gosu::Color.new(0xff000000),0)
 	  		@arial.draw(i.to_s, @px[i], @py[i], 0)
